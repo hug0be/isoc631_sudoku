dadfb2c83445bb745862ac865e47945a07601fda
hug0be <83722580+hug0be@users.noreply.github.com>
2023-03-19 19:12:46 +0100

    [Étape 1a et 1b] Ajout test création de sudoku et création de clause

--- TDD.py
+++ TDD.py
@@ -1,9 +1,68 @@
 # coding: utf-8
 from timeit import timeit
 
+from main import Sudoku, InvalidSudokuException
+
 class TestFail(Exception): pass
 
-def test_clauses_creator(): raise TestFail("Non implémenté")
+sudoku = Sudoku([
+    [3,1,6,5,7,8,4,9,2],
+    [5,2,9,1,3,4,7,6,8],
+    [4,8,7,6,2,9,5,3,1],
+    [2,6,3,4,1,5,9,8,7],
+    [9,7,4,8,6,3,1,2,5],
+    [8,5,1,7,9,2,6,4,3],
+    [1,3,8,9,4,7,2,5,6],
+    [6,9,2,3,5,1,8,7,4],
+    [7,4,5,2,8,6,3,1,9]
+])
+
+def test_sudoku_constructor():
+    try: Sudoku([])
+    except InvalidSudokuException: pass
+    try: Sudoku([[1,2,3,4,5,6,7,8,9]])
+    except InvalidSudokuException: pass
+    try: Sudoku([[1,2,3,4,5,6,7,8]])
+    except InvalidSudokuException: pass
+    try: Sudoku([[1,2,3,4,5,6,7,8,9],
+                 [1,2,3,4,5,6,7,8]])
+    except InvalidSudokuException: pass
+
+def test_clauses_creator():
+    global sudoku
+
+    # On prend un sudoku complet et on enlève le chiffre du milieu
+    sudoku[4][4] = 0
+
+    # On récupère sa fonction de clause
+    res = sudoku.clause()
+
+    # On initialise les valeurs de test (éléments | résultat attendu)
+    test_elements_answer = [
+        # Test de la 1ère et 5ᵉ ligne
+        (sudoku[0], True),
+        (sudoku[4], False),
+        # Test de la 1ère et 5ᵉ colonne
+        ([sudoku[0][i] for i in range(9)], True),
+        ([sudoku[4][i] for i in range(9)], False),
+        # Test de la 1ère et 5ᵉ région
+        ([sudoku[i][j] for i in range(3) for j in range(3)], True),
+        ([sudoku[i][j] for i in range(3,6) for j in range(3,6)], False),
+    ]
+
+    # On lance les tests
+    for elements, answer in test_elements_answer:
+        if res(elements) != answer:
+            raise TestFail(f"Une clause n'a pas retourné {answer} (éléments: {elements})")
+
+    # Test final sur l'entièreté du sudoku
+    if sudoku.valid():
+        raise TestFail(f"Le sudoku incomplet est validé")
+
+    sudoku[4][4] = 6
+    if not sudoku.valid():
+        print(sudoku)
+        raise TestFail(f"Le sudoku complet est invalidé")
 
 def test_sudoku_verifier(): raise TestFail("Non implémenté")
 
@@ -11,6 +70,7 @@ def test_sudoku_generator(): raise TestFail("Non implémenté")
 
 if __name__ == "__main__":
     tests = [
+        test_sudoku_constructor,
         test_clauses_creator,
         test_sudoku_verifier,
         test_sudoku_generator,
@@ -21,6 +81,6 @@ if __name__ == "__main__":
         try:
             print(f"Test: {test.__name__}")
             timeTaken = timeit(lambda: test(), number=1)
-            print(f"✔   Durée: {timeTaken:.2f}s ")
+            print(f"✔   Durée: {timeTaken:.2f}s")
         except TestFail as fail:
-                print(f"❌   Erreur: \"{fail}\"")
+            print(f"❌   Erreur: \"{fail}\"")
--- main.py
+++ main.py
@@ -1,3 +1,62 @@
 # coding: utf-8
+
+class InvalidSudokuException(Exception): pass
+
+class Sudoku:
+    def __init__(self, array:list[list[int]]):
+        # Validation du tableau donné
+        if len(array) != 9:
+            raise InvalidSudokuException(f"Un sudoku doit avoir 9 lignes, ici il y en a {len(array)}")
+        for i, line in enumerate(array):
+            if len(line) != 9:
+                raise InvalidSudokuException(f"Une ligne de sudoku doit avoir 9 éléments, ici, la ligne {i} en a {len(line)}")
+
+        self.array = array
+
+    def valid(self):
+        """ Valide le sudoku """
+        validation_function = self.clause()
+
+        # Validation des lignes
+        for line in self.array:
+            if not validation_function(line): return False
+
+        # Validation des colonnes
+        for column in zip(*self.array):
+            if not validation_function(column): return False
+
+        # Validation des régions
+        for i in [1,4,7]:
+            for j in [1,4,7]:
+                region = [
+                    self[i-1][j-1], self[i-1][j], self[i-1][j+1],
+                    self[i][j-1], self[i][j], self[i][j+1],
+                    self[i+1][j-1], self[i+1][j], self[i+1][j+1],
+                ]
+                if not validation_function(region): return False
+
+        return True
+
+
+    @staticmethod
+    def clause():
+        """ Retourne la fonction qui teste la validité de 9 éléments """
+        """ Traduction directe de l'implémentation Isabelle/HOL """
+        return lambda elements: all(
+            any(
+                element == digit
+                for element in elements
+            )
+            for digit in range(1,10)
+        )
+
+    def __getitem__(self, key)->list:
+        if isinstance(key, int):
+            return self.array[key]
+        raise KeyError(f"{key.__class__.__name__} n'est pas une clé valide")
+
+    def __str__(self):
+        return self.array.__str__()
+
 if __name__ == "__main__":
     pass
